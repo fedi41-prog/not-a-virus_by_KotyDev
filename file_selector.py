f338d649ebc77a3c41a7ad4fc172a0595b42db0e
fedi41-prog <crocofedi4@gmail.com>
2025-11-23 18:12:48 +0100

    fixed the bug (:

--- file_selector.py
+++ file_selector.py
@@ -1,42 +1,30 @@
-import os
+import tkinter as tk
 from tkinter import filedialog
 
-
-def check_containing(filepath, text):
-    t = read_file(filepath)
-    if text in t:
-        return True
-    return False
-
-def read_file(path):
-    try:
-        with open(path, "r") as f:
-            return f.read()
-    except Exception:
-        print("Error while reading path: " + path)
-        print("no such file")
-        return None
-
 def choose_file():
+    root = tk.Tk()
+    root.withdraw()  # versteckt das Hauptfenster
     file_path = filedialog.askopenfilename()
+    root.destroy()   # Root nach dem Dialog schließen
     return file_path
 
+def check_containing(path, text):
+    if not path:
+        return False
+    try:
+        with open(path, "r", encoding="utf8", errors="ignore") as f:
+            return text in f.read()
+    except Exception as e:
+        print("Error:", e)
+        return False
+
 def test():
-
     test_text = "helloworld"
-    path = input("press enter to choose a file with the explorer, or enter a path: ")
-    choose_file()
-    print("ok...")
-
-    if path == "":
-        path = choose_file()
-
+    path = choose_file()
     res = check_containing(path, test_text)
     if res:
         print(f"This file contains '{test_text}'!")
     else:
         print("This file is not containing the given text")
 
-
-if __name__ == "__main__":
-    test()
+test()
